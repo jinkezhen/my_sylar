b8ff408a7bdd67670e56db65996837364485c00f
jkz <2637826167@qq.com>
2025-05-17 16:46:11 +0800

    redis.cpp

---
+++ sylar/db/redis.cpp
@@ -0,0 +1,372 @@
+#include "redis.h"
+#include "sylar.h"
+#include "sylar/log.h"
+
+
+namespace sylar {
+
+static sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
+static sylar::ConfigVar<std::map<std::string, std::map<std::string, std::string> > >::ptr g_redis =
+       sylar::Config::Lookup("redis.config", std::map<std::string, std::map<std::string, std::string> >(), "redis config");
+
+static std::string get_value(const std::map<std::string, std::string>& m
+                             ,const std::string& key
+                             ,const std::string& def = "") {
+    auto it = m.find(key);
+    return it == m.end() ? def : it->second;
+}
+
+// 创建一个与输入 redisReply 对象 r 完全独立但内容相同的新对象 c
+redisReplay* RedisReplayClone(redisReplay* r) {
+    redisReplay* c = (redisReplay*)calloc(1, sizeof(*c));
+    c->type = r->type;
+    switch(r->type) {
+        case REDIS_REPLY_INTEGER:
+            c->integer = r->integer;
+            break;
+        case REDIS_REPLY_ARRAY:
+            if(r->element != NULL && r->elements > 0) {
+                c->element = (redisReply**)calloc(r->elements, sizeof(r));
+                c->elements = r->elements;
+                for(size_t i = 0; i < r->elements; ++i) {
+                    c->element[i] = RedisReplyClone(r->element[i]);
+                }
+            }
+            break;
+        case REDIS_REPLY_ERROR:
+        case REDIS_REPLY_STATUS:
+        case REDIS_REPLY_STRING:
+            if(r->str == NULL) {
+                c->str = NULL;
+            } else {
+                c->str = (char*)malloc(r->len + 1);
+                memcpy(c->str, r->str, r->len);
+                c->str[r->len] = '\0';
+            }
+            c->len = r->len;
+            break;        
+    }
+    return c;
+}
+
+Redis::Redis() {
+    m_type = IRedis::REDIS;
+}
+
+Redis::Redis(const std::map<std::string, std::string>& conf) {
+    m_type = IRedis::REDIS;
+    auto tmp = get_value(conf, "host");
+    auto pos = tmp.find(":");
+    m_host = tmp.substr(0, pos);
+    m_port = sylar::TypeUtil::Atoi(tmp.substr(pos + 1));
+    m_passwd = get_value(conf, "passwd");
+    m_logEnable = sylar::TypeUtil::Atoi(get_value(conf, "log_enable", "1"));
+
+    tmp = get_value(conf, "timeout_com");
+    if(tmp.empty()) {
+        tmp = get_value(conf, "timeout");
+    }
+    uint64_t v = sylar::TypeUtil::Atoi(tmp);
+
+    m_cmdTimeout.tv_sec = v / 1000;
+    m_cmdTimeout.tv_usec = v % 1000 * 1000;
+}
+
+
+bool Redis::reconnect() {
+    return redisReconnect(m_context.get());
+}
+
+bool Redis::connect() {
+    return connect(m_host, m_port, 50);
+}
+
+bool Redis::connect(const std::string& ip, int port, uint64_t ms) {
+    m_host = ip;
+    m_port = port;
+    m_connectMs = ms;
+    if(m_context) {
+        return true;
+    }
+    timeval tv = {(int)ms / 1000, (int)ms % 1000 * 1000};
+    auto c = redisConnectWithTimeout(ip.c_str(), port, tv);
+    if(c) {
+        if(m_cmdTimeout.tv_sec || m_cmdTimeout.tv_usec) {
+            setTimeout(m_cmdTimeout.tv_sec * 1000 + m_cmdTimeout.tv_usec / 1000);
+        }
+        m_context.reset(c, redisFree);
+
+        if(!m_passwd.empty()) {
+            auto r = (redisReply*)redisCommand(c, "auth %s", m_passwd.c_str());
+            if(!r) {
+                SYLAR_LOG_ERROR(g_logger) << "auth error:("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(r->type != REDIS_REPLY_STATUS) {
+                SYLAR_LOG_ERROR(g_logger) << "auth reply type error:" << r->type << "("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(!r->str) {
+                SYLAR_LOG_ERROR(g_logger) << "auth reply str error: NULL("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(strcmp(r->str, "OK") == 0) {
+                return true;
+            } else {
+                SYLAR_LOG_ERROR(g_logger) << "auth error: " << r->str << "("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+ReplyPtr Redis::cmd(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    ReplyPtr rt = cmd(fmt, ap);
+    va_end(ap);
+    return rt;
+}
+
+ReplyPtr Redis::cmd(const char* fmt, va_list ap) {
+    auto r = (redisReply*)redisvCommand(m_context.get(), fmt, ap);
+    if(!r) {
+        if(m_logEnable) {
+            SYLAR_LOG_ERROR(g_logger) << "redisCommand error: (" << fmt << ")(" << m_host << ":" << m_port << ")(" << m_name << ")";
+        }
+        return nullptr;
+    }
+    ReplyPtr rt(r, freeReplyObject);
+    if(r->type != REDIS_REPLY_ERROR) {
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisCommand error: (" << fmt << ")(" << m_host << ":" << m_port << ")(" << m_name << ")"
+                    << ": " << r->str;
+    }
+    return nullptr;
+}
+
+
+ReplyPtr Redis::cmd(const std::vector<std::string>& argv) {
+    std::vector<const char*> v;
+    std::vector<size_t> l;
+    for(auto& i : argv) {
+        v.push_back(i.c_str());
+        l.push_back(i.size());
+    }
+
+    auto r = (redisReply*)redisCommandArgv(m_context.get(), argv.size(), &v[0], &l[0]);
+    if(!r) {
+        if(m_logEnable) {
+            SYLAR_LOG_ERROR(g_logger) << "redisCommandArgv error: (" << m_host << ":" << m_port << ")(" << m_name << ")";
+        }
+        return nullptr;
+    }
+    ReplyPtr rt(r, freeReplyObject);
+    if(r->type != REDIS_REPLY_ERROR) {
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisCommandArgv error: (" << m_host << ":" << m_port << ")(" << m_name << ")"
+                    << r->str;
+    }
+    return nullptr;
+}
+
+ReplyPtr Redis::getReply() {
+    redisReply* r = nullptr;
+    if(redisGetReply(m_context.get(), (void**)&r) == REDIS_OK) {
+        ReplyPtr rt(r, freeReplyObject);
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisGetReply error: (" << m_host << ":" << m_port << ")(" << m_name << ")";
+    }
+    return nullptr;
+}
+
+
+int Redis::appendCmd(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int rt = appendCmd(fmt, ap);
+    va_end(ap);
+    return rt;
+}
+
+int Redis::appendCmd(const char* fmt, va_list ap) {
+    return redisvAppendCommand(m_context.get(), fmt, ap);
+}
+
+RedisCluster::RedisCluster() {
+    m_type = IRedis::REDIS_CLUSTER;
+}
+
+RedisCluster::RedisCluster(const std::map<std::string, std::string>& conf) {
+    m_type = IRedis::REDIS_CLUSTER;
+    m_host = get_value(conf, "host");
+    m_passwd = get_value(conf, "passwd");
+    m_logEnable = sylar::TypeUtil::Atoi(get_value(conf, "log_enable", "1"));
+    auto tmp = get_value(conf, "timeout_com");
+    if(tmp.empty()) {
+        tmp = get_value(conf, "timeout");
+    }
+    uint64_t v = sylar::TypeUtil::Atoi(tmp);
+
+    m_cmdTimeout.tv_sec = v / 1000;
+    m_cmdTimeout.tv_usec = v % 1000 * 1000;
+}
+
+bool RedisCluster::reconnect() {
+    return true;
+}
+
+bool RedisCluster::connect() {
+    return connect(m_host, m_port, 50);
+}
+
+bool RedisCluster::connect(const std::string& ip, int port, uint64_t ms) {
+    m_host = ip;
+    m_port = port;
+    m_connectMs = ms;
+    if(m_context) {
+        return true;
+    }
+    timeval tv = {(int)ms / 1000, (int)ms % 1000 * 1000};
+    auto c = redisClusterConnectWithTimeout(ip.c_str(), tv, 0);
+    if(c) {
+        m_context.reset(c, redisClusterFree);
+        if(!m_passwd.empty()) {
+            auto r = (redisReply*)redisClusterCommand(c, "auth %s", m_passwd.c_str());
+            if(!r) {
+                SYLAR_LOG_ERROR(g_logger) << "auth error:("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(r->type != REDIS_REPLY_STATUS) {
+                SYLAR_LOG_ERROR(g_logger) << "auth reply type error:" << r->type << "("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(!r->str) {
+                SYLAR_LOG_ERROR(g_logger) << "auth reply str error: NULL("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+            if(strcmp(r->str, "OK") == 0) {
+                return true;
+            } else {
+                SYLAR_LOG_ERROR(g_logger) << "auth error: " << r->str << "("
+                    << m_host << ":" << m_port << ", " << m_name << ")";
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+
+bool RedisCluster::setTimeout(uint64_t ms) {
+    return true;
+}
+
+ReplyPtr RedisCluster::cmd(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    ReplyPtr rt = cmd(fmt, ap);
+    va_end(ap);
+    return rt;
+}
+
+
+ReplyPtr RedisCluster::cmd(const char* fmt, va_list ap) {
+    auto r = (redisReply*)redisClustervCommand(m_context.get(), fmt, ap);
+    if(!r) {
+        if(m_logEnable) {
+            SYLAR_LOG_ERROR(g_logger) << "redisCommand error: (" << fmt << ")(" << m_host << ":" << m_port << ")(" << m_name << ")";
+        }
+        return nullptr;
+    }
+    ReplyPtr rt(r, freeReplyObject);
+    if(r->type != REDIS_REPLY_ERROR) {
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisCommand error: (" << fmt << ")(" << m_host << ":" << m_port << ")(" << m_name << ")"
+                    << ": " << r->str;
+    }
+    return nullptr;
+}
+
+ReplyPtr RedisCluster::cmd(const std::vector<std::string>& argv) {
+    std::vector<const char*> v;
+    std::vector<size_t> l;
+    for(auto& i : argv) {
+        v.push_back(i.c_str());
+        l.push_back(i.size());
+    }
+
+    auto r = (redisReply*)redisClusterCommandArgv(m_context.get(), argv.size(), &v[0], &l[0]);
+    if(!r) {
+        if(m_logEnable) {
+            SYLAR_LOG_ERROR(g_logger) << "redisCommandArgv error: (" << m_host << ":" << m_port << ")(" << m_name << ")";
+        }
+        return nullptr;
+    }
+    ReplyPtr rt(r, freeReplyObject);
+    if(r->type != REDIS_REPLY_ERROR) {
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisCommandArgv error: (" << m_host << ":" << m_port << ")(" << m_name << ")"
+                    << r->str;
+    }
+    return nullptr;
+}
+
+ReplyPtr RedisCluster::getReply() {
+    redisReply* r = nullptr;
+    if(redisClusterGetReply(m_context.get(), (void**)&r) == REDIS_OK) {
+        ReplyPtr rt(r, freeReplyObject);
+        return rt;
+    }
+    if(m_logEnable) {
+        SYLAR_LOG_ERROR(g_logger) << "redisGetReply error: (" << m_host << ":" << m_port << ")(" << m_name << ")";
+    }
+    return nullptr;
+}
+
+int RedisCluster::appendCmd(const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int rt = appendCmd(fmt, ap);
+    va_end(ap);
+    return rt;
+
+}
+
+int RedisCluster::appendCmd(const char* fmt, va_list ap) {
+    return redisClustervAppendCommand(m_context.get(), fmt, ap);
+}
+
+int RedisCluster::appendCmd(const std::vector<std::string>& argv) {
+    std::vector<const char*> v;
+    std::vector<size_t> l;
+    for(auto& i : argv) {
+        v.push_back(i.c_str());
+        l.push_back(i.size());
+    }
+    return redisClusterAppendCommandArgv(m_context.get(), argv.size(), &v[0], &l[0]);
+}
+
+
+
+}
